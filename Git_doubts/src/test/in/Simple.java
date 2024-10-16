893bf754c17ce8f6d997f5779e48c11df0173d0c
Parthasaradhi <parthasaradhi.git1506@gmail.com>
2024-10-16 19:56:55 +0530

    m2 added

--- Git_doubts/src/test/in/Simple.java
+++ Git_doubts/src/test/in/Simple.java
@@ -5,6 +5,9 @@ public class Simple {
 	 void mi(){
 		System.out.println("m1");
 	}
+	 void m2(){
+			System.out.println("m2");
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
