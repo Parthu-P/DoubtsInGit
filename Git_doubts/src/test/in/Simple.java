46589c8f4028302e115e6e75ea67d6fbb3792b68
Parthasaradhi <parthasaradhi.git1506@gmail.com>
2024-10-16 19:45:50 +0530

    m1 method added

--- Git_doubts/src/test/in/Simple.java
+++ Git_doubts/src/test/in/Simple.java
@@ -1,12 +1,16 @@
 package test.in;
 
 public class Simple {
+	
+	 void mi(){
+		System.out.println("m1");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("simple");
 
-int b=20;
+		int b=20;
 	}
 
 }
